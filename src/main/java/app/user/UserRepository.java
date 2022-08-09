ca9b9d2c43059f755da5ee9a5db9f517657d97cd
isaac harris <isaachrr4@gmail.com>
2022-08-09 15:55:06 -0400

    user almost done

--- src/main/java/app/user/UserRepository.java
+++ src/main/java/app/user/UserRepository.java
@@ -1,4 +1,18 @@
 package app.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Integer> {
+
+    boolean existsByUsername(String username);
+    //boolean existsByEmailAddress(String email);
+
+    Optional<User> findUserByUsername(String username);
+    Optional<User> findUserByEmail(String email);
+    Optional<User> findUserByUsernameAndPassword(String username, String pw);
+
 }
--- src/main/java/app/user/UserService.java
+++ src/main/java/app/user/UserService.java
@@ -1,9 +1,12 @@
 package app.user;
 
+import app.helpers.dtos.ResourceCreationResponse;
 import app.user.dtos.UserResponse;
 import app.user.dtos.NewUserRequest;
-
+import app.helpers.datasource.EntitySearcher;
 import java.util.List;
+
+import app.util.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -21,7 +24,49 @@ import java.util.stream.Collectors;
 @Transactional
 public class UserService {
 
+    private final UserRepository userRepo;
+    private final EntitySearcher entitySearcher;
 
+    @Autowired
+    public UserService(UserRepository userRepo, EntitySearcher entitySearcher) {
+        this.userRepo = userRepo;
+        this.entitySearcher = entitySearcher;
+
+    }
+
+    public List<UserResponse> fetchAllUsers() {
+        return userRepo.findAll()
+                .stream()
+                .map(UserResponse::new)
+                .collect(Collectors.toList());
+    }
+
+
+    public List<UserResponse> search(Map<String, String> requestParamMap) {
+        if (requestParamMap.isEmpty()) return fetchAllUsers();
+        Set<User> matchingUsers = entitySearcher.searchForEntity(requestParamMap, User.class);
+        if (matchingUsers.isEmpty()) throw new ResourceNotFoundException();
+        return matchingUsers.stream()
+                .map(UserResponse::new)
+                .collect(Collectors.toList());
+    }
+
+
+    public ResourceCreationResponse createUser(@Valid NewUserRequest newUserReq){
+        User newUser = newUserReq.extractResource();
+
+        if(userRepo.existsByUsername(newUser.getUsername())){
+            //throw new Exception("This username is taken");
+        }
+
+/*        int randomInt = ThreadLocalRandom.current().nextInt(0, 1000000);
+        newUser.setUserId(randomInt);*/
+
+        newUser.setRoleId(2);
+        userRepo.save(newUser);
+
+        return new ResourceCreationResponse(newUser.getUserId());
+    }
 
 
 
